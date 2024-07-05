f3fa7eff235838a327dcb46b10c062f8600758a8
Vitor Mso <vitinho1073s@gmail.com>
2024-07-05 11:13:58 -0300

    simulando conta bancaria

--- Conta-banco/ContaBanco/src/ContaTerminal.java
+++ Conta-banco/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double saldo = 237.48;
+        Scanner sc = new Scanner(System.in);
+        
+
+        System.out.println("Por favor digite o seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Por favor digite o número da Agência: ");
+        String agencia = sc.nextLine();
+
+        System.out.println("Por favor digite o número da sua conta: ");
+        int conta = sc.nextInt();
+
+        System.out.println("Olá, " + nome +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+         ", conta " + conta + " e o seu saldo " + saldo +
+          " já está disponível para saque.");
+
     }
 }
